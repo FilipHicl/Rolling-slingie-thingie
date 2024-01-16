892ed6ea0aa1810061873d873b13ee6e45c51c5e
Firgen <filip.frgelec@disroot.org>
2024-01-16 22:54:58 +0100

    teraz by to mohlo nájsť aj pružinu

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clc, clear
 
-cam = webcam;
+cam = webcam("/dev/video2");
 
 % toto zmeniť podľa potreby
 
@@ -18,38 +18,52 @@ diskErode = strel('disk', 15);
 diskDilate = strel('disk', 5);
 hBlobAnalysis = vision.BlobAnalysis(...
     'MinimumBlobArea', 600, ...
-    'MaximumBlobArea', 6000);
+    'MaximumBlobArea', 100000);
 
 
 while true
     img = cam.snapshot;
-    
+
     [red_mask, masked_red] = createMaskRed(img);
     [green_mask, masked_green] = createMaskGreen(img);
-
-    green_mask = imerode(green_mask, diskErode);
-    green_mask = imdilate(green_mask, diskDilate);
-    % green_mask = imopen(green_mask, disk);
+    [silver_mask, silver_green] = createMaskSilver(img);
 
     red_mask = imerode(red_mask, diskErode);
     red_mask = imdilate(red_mask, diskDilate);
     % red_mask = imopen(red_mask, disk);
     
+    green_mask = imerode(green_mask, diskErode);
+    green_mask = imdilate(green_mask, diskDilate);
+    % green_mask = imopen(green_mask, disk);
+
+    silver_mask = imerode(silver_mask, diskErode);
+    silver_mask = imdilate(silver_mask, diskDilate);
+    % silver_mask = imopen(silver_mask, disk);
 
     [redArea, redCentroid, redbbox] = step(hBlobAnalysis, red_mask);
     [greenArea, greenCentroid, greenbbox] = step(hBlobAnalysis, green_mask);
+    [silverArea, silverCentroid, silverbbox] = step(hBlobAnalysis, silver_mask);
+
     
     outputImage = img;
 
-    mask = green_mask+red_mask; 
+    mask = green_mask+red_mask + silver_mask; 
     outputImage(repmat(~mask,[1 1 3])) = 0;
 
     try
+        red_center = (redCentroid(1,:)+redCentroid(2,:)) ./ 2;
+        green_center = (greenCentroid(1,:)+greenCentroid(2,:)) ./ 2;
+        centers = [ red_center; green_center ];
+
         outputImage = insertShape(outputImage, 'rectangle', redbbox);
         outputImage = insertShape(outputImage, 'rectangle', greenbbox);
+        outputImage = insertShape(outputImage, 'rectangle', silverbbox);
+
 
         outputImage = insertShape(outputImage, 'line', greenCentroid);
         outputImage = insertShape(outputImage, 'line', redCentroid);
+
+        outputImage = insertShape(outputImage, 'line', centers);
     catch
     end
     
